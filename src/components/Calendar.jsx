3d9e140a26b39d19574cc17bd4cc64ba81a742aa
AniaPiWo <snowflake@wp.pl>
2023-04-18 17:01:41 +0200

    range

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { eachDayOfInterval, endOfMonth, endOfWeek, format, startOfMonth, startOfWeek, startOfToday, parse, add, sub, addDays } from "date-fns";
 import css from "./Calendar.module.css"
 
@@ -11,10 +11,9 @@ export const Calendar = () => {
   let firstDayCurrentMonth = parse(currentMonth, 'MMM yyyy', new Date())
   let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 })
   let firstDayPrevMonth = sub(firstDayCurrentMonth, { months: 1 })
-  const start = startOfWeek(startOfMonth(firstDayCurrentMonth))
+  const start = startOfWeek(firstDayCurrentMonth)
   const end = endOfMonth(firstDayCurrentMonth)
   const newStart = addDays(start, 1)
-  
 
   //let month = eachDayOfInterval({ start, end})
   //let month = eachDayOfInterval({ start: newStart, end })
@@ -34,18 +33,51 @@ export const Calendar = () => {
     setCurrentMonth(format(firstDayPrevMonth, 'MMM yyyy'))
   }
   
-  function handleDayClick(day) {
-    const index = selectedDays.findIndex(selectedDay => format(selectedDay, 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd'));
   
-    if (index === -1) {
-      setSelectedDays([...selectedDays, day]);
-    } else {
-      setSelectedDays(selectedDays.filter(selectedDay => format(selectedDay, 'yyyy-MM-dd') !== format(day, 'yyyy-MM-dd')));
+function handleDayClick(day) {
+    const selectedDayStr = format(day, 'yyyy-MM-dd');
+    
+    setSelectedDays(prevSelectedDays => {
+      const selectedDaysSorted = [...prevSelectedDays, day].sort((a, b) => a.getTime() - b.getTime());
+      const start = selectedDaysSorted[0];
+      const end = selectedDaysSorted[selectedDaysSorted.length - 1];
+      const range = eachDayOfInterval({ start, end });
+      const rangeStr = range.map(date => format(date, 'yyyy-MM-dd'));
+  
+      if (prevSelectedDays.some(selectedDay => format(selectedDay, 'yyyy-MM-dd') === selectedDayStr)) {
+        return prevSelectedDays.filter(selectedDay => format(selectedDay, 'yyyy-MM-dd') !== selectedDayStr);
+      } else if (prevSelectedDays.length > 0 && rangeStr.includes(selectedDayStr)) {
+        return range;
+      } else {
+        return [day];
+      }
+    });
+  } 
+  
+  useEffect(() => {
+    const pickedDate = document.querySelector("#picked-date");
+    if (pickedDate) {
+      const selectedDaysSorted = [...selectedDays].sort((a, b) => a.getTime() - b.getTime());
+      const selectedDaysStrArr = selectedDaysSorted.map(selectedDay => format(selectedDay, 'MMMM dd'));
+      if (selectedDaysStrArr.length === 0) {
+        pickedDate.innerHTML = "Pick date(s)";
+      } else if (selectedDaysStrArr.length === 1) {
+        pickedDate.innerHTML = `${selectedDaysStrArr[0]}`;
+      } else {
+        pickedDate.innerHTML = `${selectedDaysStrArr[0]} - ${selectedDaysStrArr[selectedDaysStrArr.length - 1]}`;
+      }
+      console.log(selectedDaysStrArr);
     }
-  }
-  
+  }, [selectedDays]);
 
+  
+  
   return (
+   <div>
+    <div className={css.header}>
+      <p>Select date</p>
+      <button id="picked-date">Pick date(s)</button>
+    </div>
     <div className={css.calendar}>
       <div className={css.nav}>
         <button  onClick={previousMonth}><p>&larr;</p>{format(firstDayPrevMonth, 'MMMMMM')}</button>
@@ -66,7 +98,7 @@ export const Calendar = () => {
       <p
         className={`${css.day} ${isSelected ? css.selectedDay : ''}`}
         key={day.toString()}
-        onClick={() => handleDayClick(day)}
+      onClick={() => handleDayClick(day)}
       >
     <time dateTime={format(day, 'yyyy-MM-dd')}>{format(day, 'd')}</time>
   </p>
@@ -74,7 +106,9 @@ export const Calendar = () => {
 
       </div>
     </div>
+    </div> 
   );
 };
+
 
 
